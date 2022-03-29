efd480c4ec7f771b1356c0e97f0bca5ddb6035c9
Phantom Cracker <popa.vlad94@gmail.com>
2022-03-29 09:30:13 -0700

    Create each city page

--- src/components/home/InfoSection.vue
+++ src/components/home/InfoSection.vue
@@ -2,14 +2,14 @@
   <div class="info-section pt-4 pb-4">
     <div class="container">
       <div class="row">
-        <div class="col-lg-6 col-12 info-section-man"></div>
         <div class="col-lg-6 col-12 ps-3">
-<!--          <h5 class="text-start">Make dreams come true   </h5>-->
+          <!--          <h5 class="text-start">Make dreams come true   </h5>-->
           <h2 class="text-start">The easiest way to turn tour home into a smart home</h2>
           <p class="text-start pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a neque vitae dui pharetra tempus nec a magna. Nulla eleifend, nisi semper iaculis condimentum, eros nulla pharetra sem, eu euismod libero risus vel mauris. Aliquam nisl tellus, dictum a volutpat non, facilisis vitae odio. Donec porttitor auctor lorem sit amet varius. Donec nec lacus ut risus laoreet euismod sit amet ac odio. Sed tincidunt vehicula metus sed pulvinar. Nunc vitae velit egestas, eleifend metus lobortis, scelerisque libero.
             Quisque sagittis iaculis blandit. Nullam arcu turpis, porttitor ac tristique eu, accumsan sed augue. Integer sodales ligula venenatis nibh convallis, ut pellentesque enim rhoncus. Donec non nibh aliquam, mattis mauris ut, semper nisl. Aenean accumsan posuere ultricies. Sed cursus ex in enim tempor, at varius est commodo. Nulla vitae elit vehicula, pulvinar eros eget, laoreet leo. Aliquam euismod tempor nisl sit amet elementum. Vivamus et massa sem. Curabitur in orci quis mauris pulvinar ultrices.
           </p>
         </div>
+        <div class="col-lg-6 col-12 info-section-man"></div>
       </div>
     </div>
   </div>
--- src/components/locksmith-near-me/CityContent.vue
+++ src/components/locksmith-near-me/CityContent.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="city">
-    Test
+    <div class="container hero">
+      <div class="row pt-4">
+        <div class="col-lg-6 col-sm-12 d-sm-none d-md-block hero-man pt-3"></div>
+        <div class="col-lg-6 col-sm-12 d-flex align-self-center flex-column mt-3 mt-md-0 pb-4">
+          <h1 class="text-white text-start">Emergency <span class="my-yellow">Locksmith</span> service in {{ city }}</h1>
+          <ul class="with-border bg-black">
+            <li class="text-white text-start pl-2 pt-3">Service on spot withing 25 minutes</li>
+            <li class="text-white text-start pl-2">Local service from £49</li>
+            <li class="text-white text-start pl-2 pb-3">Locks from £25</li>
+          </ul>
+          <button class="btn btn-primary w-50 my-primary-btn mt-3">
+            <a class="text-white text-decoration-none" href="tel:03333446467"><font-awesome-icon icon="phone-alt" style="margin-right: 10px" />Call now</a>
+          </button>
+        </div>
+      </div>
+    </div>
+    <InfoSection />
+    <OurServices />
+    <TestimonialsContent />
   </div>
 </template>
 
 <script>
+import InfoSection from "@/components/home/InfoSection";
+import TestimonialsContent from "@/components/home/TestimonialsContent";
+import OurServices from "@/components/home/OurServices";
 export default {
   name: "CityContent",
+  components: {OurServices, TestimonialsContent, InfoSection},
   data() {
     let city = this.$route.params.city
     return {
@@ -17,6 +39,50 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.hero {
+  background-image: url("../../../public/images/hero-background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  max-height: 500px;
+  border-bottom: 10px solid #000000;
 
+  @media (max-width: 700px) {
+    max-height: 100%;
+  }
+  .hero-man {
+    background-image: url("../../../public/images/header-man.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    @media (max-width: 700px) {
+      display: none;
+    }
+  }
+  li {
+    font-family: 'Ubuntu', sans-serif;
+  }
+}
+h1 {
+  font-size: 2.5rem;
+  &.with-border {
+    border-left: 10px solid $ourRed;
+    padding-left: 25px;
+    @media (max-width: 700px) {
+      font-size: 3.5rem;
+    }
+  }
+}
+.btn-primary {
+  background-color: $ourRed !important;
+  border: none;
+  &:focus, :active, :target, :visited {
+    border: none;
+    box-shadow: none;
+
+  }
+  a {
+    text-align: center;
+  }
+}
 </style>
