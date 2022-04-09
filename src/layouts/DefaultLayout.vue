8adea1bbf51bd24ef4b9227febc67f8548c2db72
Phantom Cracker <popa.vlad94@gmail.com>
2022-04-09 06:52:37 -0700

    Update DefaultLayout.vue

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -31,22 +31,22 @@
           <div class="row">
             <div class="col-6 col-lg-2">
               <div class="nav-item badge">
-                <img src="../../public/images/services/247-service-badge.png" alt="badge1" width="100px"/>
+                <img src="../../public/images/services/247-service-badge.png" alt="badge1" width="70px"/>
               </div>
             </div>
             <div class="col-6 col-lg-2">
               <div class="nav-item badge me-lg-5">
-                <img src="../../public/images/services/toprated.png" alt="badge1" width="100px"/>
+                <img src="../../public/images/services/toprated.png" alt="badge1" width="70px"/>
               </div>
             </div>
             <div class="col-6 col-lg-2 pt-4 pt-lg-0">
               <div class="nav-item badge me-lg-5">
-                <img src="../../public/images/services/warranty.png" alt="badge1" width="100px"/>
+                <img src="../../public/images/services/warranty.png" alt="badge1" width="70px"/>
               </div>
             </div>
             <div class="col-6 col-lg-2 pt-4 pt-lg-0">
               <div class="nav-item badge me-lg-5">
-                <img src="../../public/images/services/warranty.png" alt="badge1" width="100px"/>
+                <img src="../../public/images/services/warranty.png" alt="badge1" width="70px"/>
               </div>
             </div>
             <div class="col-12 col-lg-4 pt-lg-0 align-self-center">
