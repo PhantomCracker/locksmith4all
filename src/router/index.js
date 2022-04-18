5357b23e73e605419ce07b643c193f6100019f87
Popa Vlad <popa.vlad94@gmail.com>
2022-04-18 21:21:59 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -396,7 +396,7 @@ export  default new Router({
                    component: WatfordCity
                },
                {
-                   path: '([/]*.*)*',
+                   path: '(.*)*$',
                    component: NotFound,
                    name: 'NotFound'
                }
