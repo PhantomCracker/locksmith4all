95f9988145a75705533b5b6c539b20dee474d9b4
Popa Vlad <popa.vlad94@gmail.com>
2024-02-23 17:23:03 +0200

    fix images

--- src/components/home/BadgesContent.vue
+++ src/components/home/BadgesContent.vue
@@ -4,17 +4,17 @@
       <div class="row">
         <div class="col-4">
           <div class="nav-item badge">
-            <img src="../../../public/images/services/247-service-badge.webp" alt="badge1" width="100px"/>
+            <img src="../../../public/images/services/247-service-badge.webp" alt="badge1"/>
           </div>
         </div>
         <div class="col-4">
           <div class="nav-item badge me-lg-5">
-            <img src="../../../public/images/services/toprated.webp" alt="badge1" width="100px"/>
+            <img src="../../../public/images/services/toprated.webp" alt="badge1"/>
           </div>
         </div>
         <div class="col-4">
           <div class="nav-item badge me-lg-5">
-            <img src="../../../public/images/services/warranty.webp" alt="badge1" width="100px"/>
+            <img src="../../../public/images/services/warranty.webp" alt="badge1"/>
           </div>
         </div>
       </div>
@@ -32,5 +32,11 @@ export default {
 .badges {
   background-color: $ourBlack;
   border-bottom: solid 10px black;
+}
+
+.nav-item {
+  img {
+    width: 100px;
+  }
 }
 </style>
